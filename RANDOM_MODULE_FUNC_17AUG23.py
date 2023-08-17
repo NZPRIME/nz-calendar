f0ea2ef21325f16530c26677efecfdf4fc57a822
NAWAZISH FAROOQUI <141997208+NZPRIME@users.noreply.github.com>
2023-08-17 08:23:39 +0530

    Add files via upload
    RANDOM FUNCTION EXECUTION USING PYTHON

---
+++ RANDOM_MODULE_FUNC_17AUG23.py
@@ -0,0 +1,13 @@
+import random
+l=['0','1','2','3','4','5','6','7','8','9']
+otp=" "
+for i in range(6):
+    otp=otp + random.choice(l)
+print(otp)
+print("====================")
+import random
+l=['0','1','2','3','4','5','6','A','B','C','D','E','F']
+var=" "
+for i in range(6):
+    var=var + random.choice(l)
+print(var)
